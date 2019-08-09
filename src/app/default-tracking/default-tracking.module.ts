ed961fd552c11b59ccc19e08fa282905a14e6300
szymongretka <39966456+szymongretka@users.noreply.github.com>
2019-08-09 09:44:24 +0200

    update

---
+++ src/app/default-tracking/default-tracking.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DefaultTrackingRoutingModule } from './default-tracking-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DefaultTrackingRoutingModule
+  ]
+})
+export class DefaultTrackingModule { }
